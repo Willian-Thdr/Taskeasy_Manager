914d78ea73c93f466895cc085bff0622299df13f
Willian Theodoro <business.will.theo@gmail.com>
2026-08-21 19:34:29 -0300

    Criando base do projeto

--- Source/Models/MainWindow.axaml.cs
+++ Source/Models/MainWindow.axaml.cs
@@ -1,11 +1,22 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Taskeasy_Manager.Source.Models;
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        this.WindowState = WindowState.Maximized;
         InitializeComponent();
+    }
+
+    public void CreateTask(object sender, RoutedEventArgs args)
+    {
+        Console.WriteLine("Criar");
+    }
+
+    public void LoadTask(object sender, RoutedEventArgs args)
+    {
+        Console.WriteLine("Carregar");
     }
 }
