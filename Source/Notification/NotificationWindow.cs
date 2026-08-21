7ee9d3f11261a335bf6bc9f3cd731d0c5d599ad1
Willian Theodoro <business.will.theo@gmail.com>
2026-08-21 19:35:36 -0300

    feat
    Adicionando mais pastas ao projeto
    
    Criando janela de notificações

---
+++ Source/Notification/NotificationWindow.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Text.RegularExpressions;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+public class NotificationWindow
+{
+    public static void Message(String text)
+    {
+        Window message = new Window
+        {
+            Width = 350,
+            Height = 300,
+            Background = new SolidColorBrush(Color.Parse("#d6ceb6")),
+            Title = "Message Box"
+        };
+
+        Grid grid = new Grid
+        {
+            Margin = new Avalonia.Thickness(20)
+        };
+
+        Label txt = new Label
+        {
+            Content = text,
+            FontSize = 16,
+            Foreground = Brushes.Black,
+            Margin = new Avalonia.Thickness(5),
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center
+        };
+
+        grid.Children.Add(txt);
+
+        message.Content = grid;
+
+        message.Show();
+    }
+}
